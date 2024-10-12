d8ce49daef5fd83104bcb3ad82a892c99259cfb6
Yclaze <teralocsi@gmail.com>
2024-10-12 10:01:23 +0200

    tesztEsetek()

---
+++ src/test/java/hu/petloc/modell/EmberTest.java
@@ -0,0 +1,33 @@
+package hu.petloc.modell;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmberTest {
+
+    @BeforeEach
+    void setUp() {
+        tesztEsetek();
+    }
+
+    private void tesztEsetek() {
+    }
+
+    @Test
+    void setNyelvSzam() {
+    }
+
+    @Test
+    void nyelvHozzaAd() {
+    }
+
+    @Test
+    void isIdegenNyelv() {
+    }
+
+    @Test
+    void valaszol() {
+    }
+}
